113214231e42c596c481db91edabfbb0a675d342
Irfan Alamsyah <irfanalamsyah2@gmail.com>
2024-12-27 09:58:47 +0700

    feat: implement admin authentication for hardware operations and enhance unauthorized access handling

--- src/handlers/hardwares.rs
+++ src/handlers/hardwares.rs
@@ -1,7 +1,8 @@
 use ntex::http::{Request, Response, StatusCode};
 use ntex::web::Error;
 
-use crate::utils::auth::authenticate;
+use crate::constant::messages::MESSAGE_UNAUTHORIZED;
+use crate::utils::auth::{authenticate, authenticate_admin};
 use crate::utils::http::extract_id_from_path;
 use crate::{app::App, utils::http::response_json};
 
@@ -17,12 +18,13 @@ impl App {
     }
 
     pub async fn handle_post_hardwares(&self, mut req: Request) -> Result<Response, Error> {
-        match authenticate(&req).await {
+        match authenticate_admin(&req).await {
             Ok(_) => {
                 let payload = req.payload();
                 let (data, status) = self.0.add_hardware(payload).await;
                 Ok(response_json(data, status))
             }
+            Err(err) if err == MESSAGE_UNAUTHORIZED => self.handle_not_authorized(req).await,
             Err(err) => self.handle_not_authenticated_with_message(req, err).await,
         }
     }
@@ -42,12 +44,13 @@ impl App {
 
     pub async fn handle_update_hardware(&self, mut req: Request) -> Result<Response, Error> {
         match extract_id_from_path(req.path(), "/hardwares/") {
-            Some(id) => match authenticate(&req).await {
+            Some(id) => match authenticate_admin(&req).await {
                 Ok(_) => {
                     let payload = req.payload();
                     let (data, status) = self.0.update_hardware(id, payload).await;
                     Ok(response_json(data, status))
                 }
+                Err(err) if err == MESSAGE_UNAUTHORIZED => self.handle_not_authorized(req).await,
                 Err(err) => self.handle_not_authenticated_with_message(req, err).await,
             },
             None => Ok(Response::new(StatusCode::BAD_REQUEST)),
@@ -56,11 +59,12 @@ impl App {
 
     pub async fn handle_delete_hardware(&self, req: Request) -> Result<Response, Error> {
         match extract_id_from_path(req.path(), "/hardwares/") {
-            Some(id) => match authenticate(&req).await {
+            Some(id) => match authenticate_admin(&req).await {
                 Ok(_) => {
                     let (data, status) = self.0.delete_hardware(id).await;
                     Ok(response_json(data, status))
                 }
+                Err(err) if err == MESSAGE_UNAUTHORIZED => self.handle_not_authorized(req).await,
                 Err(err) => self.handle_not_authenticated_with_message(req, err).await,
             },
             None => Ok(Response::new(StatusCode::BAD_REQUEST)),
