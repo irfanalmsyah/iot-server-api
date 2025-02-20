206e02f14e53304f3bf03eaadfbaa64bf424b86b
Irfan Alamsyah <irfanalamsyah2@gmail.com>
2025-02-20 09:20:24 +0700

    feat: refactor database connection setup in AppFactory and main function for improved clarity and performance

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use app::AppFactory;
+use deadpool_postgres::{Config, ManagerConfig, Pool, RecyclingMethod, Runtime};
 use ntex::http::{HttpService, KeepAlive::Os};
 use ntex::server;
 use ntex::{time::Seconds, util::PoolId, util::Ready};
 use std::io::Result as IoResult;
 use std::sync::{Arc, Mutex};
+use tokio_postgres::NoTls;
 
 mod app;
 mod constant;
@@ -24,9 +26,26 @@ async fn main() -> IoResult<()> {
     let total_cores = cores.len();
     let cores = Arc::new(Mutex::new(cores));
 
+    let mut cfg_pool = Config::new();
+    cfg_pool.dbname = Some("rustdemo".to_string());
+    cfg_pool.user = Some("postgres".to_string());
+    cfg_pool.password = Some("password".to_string());
+    cfg_pool.host = Some("localhost".to_string());
+    cfg_pool.manager = Some(ManagerConfig {
+        recycling_method: RecyclingMethod::Fast,
+    });
+    cfg_pool.get_pool_config().max_size = 50;
+
+    let pool: Pool = cfg_pool
+        .create_pool(Some(Runtime::Tokio1), NoTls)
+        .expect("Failed to create pool");
+
+    let pool = Arc::new(pool);
+
     server::build()
         .backlog(1024)
         .bind("techempower", "0.0.0.0:8080", {
+            let pool = pool.clone();
             move |cfg| {
                 cfg.memory_pool(PoolId::P1);
                 PoolId::P1.set_read_params(65535, 2048);
@@ -37,7 +56,7 @@ async fn main() -> IoResult<()> {
                     .client_timeout(Seconds(0))
                     .headers_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
                     .payload_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
-                    .h1(AppFactory)
+                    .h1(AppFactory { pool: pool.clone() })
             }
         })?
         .configure(move |cfg| {
